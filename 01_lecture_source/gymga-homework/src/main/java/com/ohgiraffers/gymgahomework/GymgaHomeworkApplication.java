2ab64e7707f56ff4e2049043c472df7970be13d0
SOOBIN\tifkr <soobinnunu1101@gmail.com>
2024-04-29 22:45:01 +0900

    0429 home commit

---
+++ 01_lecture_source/gymga-homework/src/main/java/com/ohgiraffers/gymgahomework/GymgaHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.gymgahomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymgaHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymgaHomeworkApplication.class, args);
+    }
+
+}
---
+++ 01_lecture_source/gymga-homework/src/test/java/com/ohgiraffers/gymgahomework/GymgaHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.gymgahomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymgaHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
