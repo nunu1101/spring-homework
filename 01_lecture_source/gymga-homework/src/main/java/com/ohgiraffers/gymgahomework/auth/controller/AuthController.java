a6cedbe6f9b4cc3eee9a2298a1d1b9541b168ab3
SOOBIN\tifkr <soobinnunu1101@gmail.com>
2024-05-01 18:10:40 +0900

    0501 commit2

--- 01_lecture_source/gymga-homework/src/main/java/com/ohgiraffers/gymgahomework/auth/controller/AuthController.java
+++ 01_lecture_source/gymga-homework/src/main/java/com/ohgiraffers/gymgahomework/auth/controller/AuthController.java
@@ -1,7 +1,12 @@
 package com.ohgiraffers.gymgahomework.auth.controller;
 
+import com.ohgiraffers.gymgahomework.user.model.dto.LoginDTO;
+import com.ohgiraffers.gymgahomework.user.model.dto.SignUpDTO;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -21,4 +26,12 @@ public class AuthController {
 
         return mv;
     }
+
+//    @PostMapping("find")
+//        public String serachId(HttpServletRequest req, Model model, SignUpDTO signUpDTO,
+//                               @RequestParam String userName,
+//                               @RequestParam String userPhone) {
+//                signUpDTO.setUserName(userName);
+//    }
+//    return;
 }
